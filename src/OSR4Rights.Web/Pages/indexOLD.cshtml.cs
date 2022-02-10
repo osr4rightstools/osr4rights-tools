d7ad0fac0b2b5cfa4052bf62eed906093aaace18
Dave Mateer <davemateer@gmail.com>
2022-02-10 13:01:25 +0000

    auto

---
+++ src/OSR4Rights.Web/Pages/indexOLD.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OSR4Rights.Web.Pages
+{
+    public class IndexModelOLD : PageModel
+    {
+        public async Task OnGet()
+        {
+        }
+    }
+}
---
+++ src/OSR4Rights.Web/wwwroot/js/script.js
@@ -0,0 +1,11 @@
+﻿const menuButton = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+function navToggle() {
+    // adds / removes open class from menu-btn element
+    menuButton.classList.toggle('open');
+    mobileMenu.classList.toggle('hidden');
+    document.body.classList.toggle('no-scroll');
+}
+
+menuButton.addEventListener('click', navToggle)
