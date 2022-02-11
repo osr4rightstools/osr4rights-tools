245734800ac6051b9350ca35721251d70b6f6fdd
Dave Mateer <davemateer@gmail.com>
2022-02-11 10:44:03 +0000

    auto

---
+++ src/OSR4Rights.Web/wwwroot/js/result.js
@@ -0,0 +1,8 @@
+﻿const showLogsButton = document.getElementById('show-logs-button');
+const logsList = document.getElementById('logs-list');
+
+function toggle() {
+    logsList.classList.toggle('hidden-display-none');
+}
+
+showLogsButton.addEventListener('click', toggle)
