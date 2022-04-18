1ac7eb7abf6fb565bd398ad7168e91054774f131
Dave Mateer <davemateer@gmail.com>
2022-04-18 09:24:51 +0100

    auto

--- src/OSR4Rights.Web/Pages/Index.cshtml.cs
+++ src/OSR4Rights.Web/Pages/Index.cshtml.cs
@@ -2,13 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace OSR4Rights.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public async Task OnGet()
+        public string? HsResult { get; set; }
+
+        public async Task OnGet(string? q)
         {
+            if (q == null) { }
+            else
+            {
+                Log.Information($"q is {q}");
+                HsResult = "Contains hate";
+            }
         }
     }
 }
