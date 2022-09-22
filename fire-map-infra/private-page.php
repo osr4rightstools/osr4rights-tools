ffae1de407b3630e2447992224d455d99a520a19
Dave Mateer <davemateer@gmail.com>
2022-09-22 09:54:20 +0100

    auto

--- fire-map-infra/private-page.php
+++ fire-map-infra/private-page.php
@@ -1,53 +1,65 @@
-<p>private-page!</p>
-<br />
+<p>private-page</p>
 
 <?php
+
+function exception_handler($exception)
+{
+	echo "<h1>Failure</h1>";
+	echo "Exception: ", $exception->getMessage();
+	echo "<h1>PHP Info for troubleshooting</h1>";
+	phpinfo();
+}
+# default exception handler if not within a try/catch
+# eg when database is down or wrong connection string
+set_exception_handler('exception_handler');
+
+function formatErrors($errors)
+{
+	echo "<h1>SQL Error:</h1>";
+	echo "Error information: <br/>";
+	foreach ($errors as $error) {
+		echo "SQLSTATE: " . $error['SQLSTATE'] . "<br/>";
+		echo "Code: " . $error['code'] . "<br/>";
+		echo "Message: " . $error['message'] . "<br/>";
+	}
+}
+
 //
-// Get Cookie from ASPNET
+// Get Cookie 
 //
 $cookie_name = "_AspNetCore_Cookies";
 
+// $_COOKIE[$cookie_name] = "for testing put a real cookie in here";
+
 if (!isset($_COOKIE[$cookie_name])) {
-	// no cookie, so not authenticated
 	echo "Cookie named '" . $cookie_name . "' is not set! You should not be here. Redirect to /fire-map perhaps";
 } else {
-	// have a cookie with the correct name (which could be forged)
 
 	$cookie_value = $_COOKIE[$cookie_name];
-	echo "_AspNetCore_Cookies found for https://osr4rightstools.org and Value is: " . $cookie_value;
 
 	//
 	// Call database to see if this is a real cookie that we know about
-	// read connection string from .ENV variable?
 	//
-	// also the email, loginId, RoleId (eg 1 - Tier 1, 2 - Tier 2, 9 - Admin)
-
-	// read pwd from env variable set on server build - secrets never checked into source
 
 	// env variables are set in /etc/apache2/envvars
 	// which are copied in the deploy script to the server 
-	// in /sercrets/fire-map-aoache-envvars.sh
 	$serverName = getenv('DB_SERVERNAME');
 	$database = getenv('DB_DATABASE');
 	$username = getenv('DB_USERNAME');
 	$password = getenv('DB_PASSWORD');
 
+	// localhost testing
+	// $serverName = "127.0.0.1";
+	// $database = "OSR4Rights" ;
+	// $username = "sa";
+	// $password = "zpsecret";
+
 	$connectionOptions = array(
 		"database" => $database,
 		"uid" => $username,
-		"pwd" => $password
+		"pwd" => $password,
+		"TrustServerCertificate" => true
 	);
-
-	function exception_handler($exception)
-	{
-		echo "<h1>Failure</h1>";
-		echo "Exception: ", $exception->getMessage();
-		echo "<h1>PHP Info for troubleshooting</h1>";
-		phpinfo();
-	}
-	# default exception handler if not within a try/catch
-	# eg when database is down or wrong connection string
-	set_exception_handler('exception_handler');
 
 	// Establishes db connection
 	$conn = sqlsrv_connect($serverName, $connectionOptions);
@@ -55,15 +67,15 @@ if (!isset($_COOKIE[$cookie_name])) {
 		die(formatErrors(sqlsrv_errors()));
 	}
 
-	//
-	// Select Query
-	//
+	$tsql = "
+	SELECT c.LoginId, l.Email 
+	FROM Cookie c 
+	JOIN Login l on c.LoginId = l.LoginId
+	WHERE CookieValue = ? 
+	AND ExpiresUtc > GETUTCDATE()";
 
-	$tsql = "SELECT LoginId FROM Cookie WHERE CookieValue = ? AND ExpiresUtc > GETUTCDATE()";
-	// $params = array("asdfjkahsdkjfhasdf");
 	$params = array($cookie_value);
-
-	$stmt = sqlsrv_query($conn, $tsql, $params);
+	$stmt = sqlsrv_query($conn, $tsql, $params, array("Scrollable" => 'static'));
 
 	if ($stmt === false) {
 		die(formatErrors(sqlsrv_errors()));
@@ -71,7 +83,6 @@ if (!isset($_COOKIE[$cookie_name])) {
 
 	$row_count = sqlsrv_num_rows($stmt);
 
-	echo "rows returned is: " . $row_count;
 	if ($row_count == 0) {
 		echo "not authenticated!";
 	} else {
@@ -79,36 +90,13 @@ if (!isset($_COOKIE[$cookie_name])) {
 
 		while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
 			$loginId = $row['LoginId'];
-			echo "loginId is " . $loginId;
-
-			// echo $row['SQL_VERSION'] . PHP_EOL;
+			$email = $row['Email'];
+			echo "<br/><br/>loginId is " . $loginId;
+			echo "<br/><br/>email is " . $email;
 		}
 	}
-
-
-	// OLD
-	// echo "<h1> Success Results : </h1>";
-
-	// while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-	// 	echo $row['SQL_VERSION'] . PHP_EOL;
-	// }
 
 	sqlsrv_free_stmt($stmt);
 	sqlsrv_close($conn);
-
-
-	// where can this function be.. above the error handler?
-	function formatErrors($errors)
-	{
-		// Display errors
-		echo "<h1>SQL Error:</h1>";
-		echo "Error information: <br/>";
-		foreach ($errors as $error) {
-			echo "SQLSTATE: " . $error['SQLSTATE'] . "<br/>";
-			echo "Code: " . $error['code'] . "<br/>";
-			echo "Message: " . $error['message'] . "<br/>";
-		}
-	}
 }
-
 ?>
--- fire-map-infra/public-page.php
+++ fire-map-infra/public-page.php
@@ -2,5 +2,7 @@ Hello this is a public-page!
 <br />
 
 <?php
+$test = "hello";
+
 echo "The time is " . date("h:i:sa");
 ?>
