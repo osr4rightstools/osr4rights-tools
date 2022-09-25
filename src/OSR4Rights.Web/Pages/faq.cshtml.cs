52762d3088f0e367b8c89191fd2af53622bf169a
Dave Mateer <davemateer@gmail.com>
2022-09-25 09:04:38 +0100

    auto

---
+++ src/OSR4Rights.Web/Pages/faq.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OSR4Rights.Web.Pages
+{
+    public class FaqModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
